bba2e6b06c78c978984ef62b34f98d4c30a62547
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2020-11-22 09:04:57 -0800

    Cleanup and add more data

--- node_component/index.js
+++ node_component/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:095b47e1606161ad331389f0c09adf907f0302c7b49d77544befb35843153852
-size 776
+oid sha256:9fbbfb449eecdcb3d8e4bfa4c367252adfc25090e99b3cef78a14cb562a20601
+size 867
