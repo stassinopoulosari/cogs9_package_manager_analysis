23a9db28dee5d748ca9b89a9ee3e5dac8e78a10b
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2020-11-22 10:10:28 -0800

    Another try with LFS

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4b7659d15327167247dd5409a6fb60c2a8d22d40398a0b8c65db14dad6e2469a
-size 203
+# cogs9_package_data_analysis
+
+##To run
+
+cd into main directory, run `jupyter notebook` and click into `cogs9_package_data_analysis.ipynb`
+
+## Required Packages
+
+- pandas
+- numpy
+- matplotlib
+- requests
--- node_component/index.js
+++ node_component/index.js
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9fbbfb449eecdcb3d8e4bfa4c367252adfc25090e99b3cef78a14cb562a20601
-size 867
+const axios = require("axios"),
+fs = require("fs"),
+registry = require('package-stream')({
+  db: 'https://replicate.npmjs.com',
+  include_docs: true
+});
+if(!fs.existsSync("../data/npm_csv.csv")) {
+fs.writeFileSync("../data/npm_csv.csv","name,uri");
+var packages = [];
+registry
+  .on('package', (package) =>{ //console.log(package);
+     fs.appendFile("../data/npm_csv.csv","\n" + package.name + "," +
+       (package.other && package.other.dist ? package.other.dist.tarball : "null"),
+     () => {console.log("+");})
+     packages.push({
+       name: package.name,
+       package_uri:
+         (package.other && package.other.dist ? package.other.dist.tarball : null)
+    });
+    console.log(packages.length);})
+  .on('up-to-date', () => {
+    fs.writeFileSync("../data/npm_json.json",JSON.stringify(packages))});
+} else {
+console.log("npm_csv.csv already exists")
+}
